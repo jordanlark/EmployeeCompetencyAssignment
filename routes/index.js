f809919476baab3644d3adcce09088d9ba6528d5
Jordan <jordanlarkin24@gmail.com>
2015-02-26 14:15:23 -0700

    commit changes for assignment 2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Awesome App' });
 });
 
+router.get('/', function(req, res, next) {
+  res.send('Post is working!');
+});
+
+
+router.post('/api', function(req, res, next){
+  console.log(req.body.username);
+  console.log(req.body.email);
+  res.send(req.body);
+});
+
+
 module.exports = router;
